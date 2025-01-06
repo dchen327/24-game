12cdcdd8e99b5c0455b80711114b6d379d45ff6e
David Chen <davidc3287@gmail.com>
2025-01-06 13:58:10 -0500

    setup for pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // PWA assets (service worker) will be generated in the public folder
+  register: true, // Automatically register the service worker
+  skipWaiting: true, // Activate the new service worker as soon as it's available
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development
+  ...nextConfig, // Merge other Next.js config options
+});
