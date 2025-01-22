2dcb96a3091f78ad0965e283b93850cf11dbe374
David Chen <davidc3287@gmail.com>
2025-01-21 21:20:05 -0800

    tooltips

--- src/components/settings-modal.tsx
+++ src/components/settings-modal.tsx
@@ -16,6 +16,12 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { Slider } from "./ui/slider";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { InfoIcon } from "lucide-react";
 
 type SettingsModalProps = {
   open: boolean;
@@ -78,6 +84,17 @@ export function SettingsModal({
               className="col-span-2 font-semibold text-xl"
             >
               Autocomplete:
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant="ghost" className="h-8 w-8 p-0">
+                    <InfoIcon className="h-5 w-5" />
+                    <span className="sr-only">Autocomplete Info</span>
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-full border border-black p-2">
+                  <p>Autocomplete last step</p>
+                </PopoverContent>
+              </Popover>
             </Label>
             <div className="flex justify-end">
               <Checkbox
@@ -90,12 +107,22 @@ export function SettingsModal({
             </div>
           </div>
           <div className="grid grid-cols-3 items-center gap-4">
-            <Label
-              htmlFor="randomProb"
-              className="col-span-2 font-semibold text-xl"
-            >
-              Random Probability:
-            </Label>
+            <div className="col-span-2 flex items-center gap-2">
+              <Label htmlFor="randomProb" className="font-semibold text-xl">
+                Random Probability:
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <InfoIcon className="h-5 w-5" />
+                      <span className="sr-only">Random Probability Info</span>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-full border border-black p-2">
+                    <p>Probability of getting an easier problem</p>
+                  </PopoverContent>
+                </Popover>
+              </Label>
+            </div>
             <div className="text-xl text-right">{randomProbForm[0]}</div>
             <Slider
               id="randomProb"
