f84be7a7857eeaca58c5e6fc69e1fa58d8fefda1
David Chen <davidc3287@gmail.com>
2025-03-13 14:56:58 -0700

    remove unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const APP_NAME = "24 Game";
 const APP_DEFAULT_TITLE = "24 Game";
